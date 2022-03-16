6e0fe649466c28a4413afc052cdaea4dffb3d85a
Kurt Maglalang <kbm126@live.com>
2022-03-16 19:52:44 +0000

    fixed word search function to look vertical and horizontal

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let result = [];
+  
+  matrix.forEach((element) => {
+    element.forEach((elm,ind) => {
+      if (result[ind] === undefined) {
+        result.push([]);
+      }
+      result[ind].push(elm);
+    });
+  });
+  
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+  // horizontal check
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // vertical check
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
